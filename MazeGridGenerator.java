84057b46d4a0297b59ecfb645419d69bbb89a963
MilanVersteegh <milanversteegh@icloud.com>
2024-10-16 13:13:32 +0200

    mjhkh

--- MazeGridGenerator.java
+++ MazeGridGenerator.java
@@ -26,34 +26,35 @@ public class MazeGridGenerator {
             }
         });
 
-    private void generateMaze() {
+        private void generateMaze() {
 
-        for (int row = 0; row < GR; row++) {
-            for (int col = 0; col < GC; col++) {
-                grid[row][col] = 0; 
-            }
-        }
-
-        for (int row = 1; row < GR - 1; row += 2) {
-            for (int col = 1; col < GC - 1; col += 2) {
-                if (grid[row][col] == 0) {
-                    grid[row][col] = 1; // Path
+            for (int row = 0; row < GR; row++) {
+                for (int col = 0; col < GC; col++) {
+                    grid[row][col] = 0; 
                 }
             }
+
+            for (int row = 1; row < GR - 1; row += 2) {
+                for (int col = 1; col < GC - 1; col += 2) {
+                    if (grid[row][col] == 0) {
+                        grid[row][col] = 1; // Path
+                    }
+                }
+            }
+
+            int cellSizeRow = (GR - 1) / 2;
+            int cellSizeCol = (GC - 1) / 2;
+            visited = new boolean[cellSizeRow][cellSizeCol];
+
+            Random rand = new Random();
+            int startX = rand.nextInt(cellSizeRow);
+            int startY = rand.nextInt(cellSizeCol);
+
+            // Start maze generation from a random cell
+            generateMazeRecursive(startX, startY);
+
+            // Create loops to have multiple paths
+            createLoops(10); // Adjust the number for more or fewer loops
         }
-
-        int cellSizeRow = (GR - 1) / 2;
-        int cellSizeCol = (GC - 1) / 2;
-        visited = new boolean[cellSizeRow][cellSizeCol];
-
-        Random rand = new Random();
-        int startX = rand.nextInt(cellSizeRow);
-        int startY = rand.nextInt(cellSizeCol);
-
-        // Start maze generation from a random cell
-        generateMazeRecursive(startX, startY);
-
-        // Create loops to have multiple paths
-        createLoops(10); // Adjust the number for more or fewer loops
     }
 }
